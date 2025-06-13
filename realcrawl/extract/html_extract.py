faa1b52ba8fbaa96e2a9b272e82728fa9a8e5994
dt-yy <qywan918@163.com>
2025-06-13 21:51:49 +0800

    fix pylint

--- realcrawl/extract/html_extract.py
+++ realcrawl/extract/html_extract.py
@@ -1,39 +1,39 @@
 
-from llm_web_kit.input.datajson import DataJson
+from func_timeout import func_timeout
 from llm_web_kit.extractor.extractor_chain import ExtractSimpleFactory
-from func_timeout import FunctionTimedOut, func_timeout
-from llm_web_kit.exception.exception import *
+from llm_web_kit.input.datajson import DataJson
+
 from realcrawl.cfg import load_pipe_tpl
 
 
 class HtmlExtract:
-    def __init__(self, html_file_path: str, output_format: str = "md"):
-        self.config = load_pipe_tpl("extractor_pipe")
-        self.extractor_chain =  ExtractSimpleFactory.create(self.config)
+    def __init__(self, html_file_path: str, output_format: str = 'md'):
+        self.config = load_pipe_tpl('extractor_pipe')
+        self.extractor_chain = ExtractSimpleFactory.create(self.config)
         self.d = {
-            "track_id": "1",
-            "html": open(html_file_path, "r").read(),
-            "url": "https://www.google.com",
-            "domain": "google.com",
-            "dataset_name":"cc",
-            "data_source_category":"HTML",
-            "file_bytes": 4096,
-            "page_layout_type": "article",
-            "meta_info": {"input_datetime": "2020-01-01 00:00:00"}
-        }        
+            'track_id': '1',
+            'html': open(html_file_path, 'r').read(),
+            'url': 'https://www.google.com',
+            'domain': 'google.com',
+            'dataset_name':'cc',
+            'data_source_category':'HTML',
+            'file_bytes': 4096,
+            'page_layout_type': 'article',
+            'meta_info': {'input_datetime': '2020-01-01 00:00:00'}
+        }
         self.output_format = output_format
 
     def get_html_content(self):
-        print("self.d: ", self.d)
+        print('self.d: ', self.d)
         input_data = DataJson(self.d)
         data_e: DataJson = func_timeout(10, self.extractor_chain.extract, args=(input_data,))
-        print("data_e: ", data_e.get_content_list().to_json())
-        if self.output_format == "md":
+        print('data_e: ', data_e.get_content_list().to_json())
+        if self.output_format == 'md':
             md_content = data_e.get_content_list().to_mm_md()
-        elif self.output_format == "json":
+        elif self.output_format == 'json':
             md_content = data_e.get_content_list().to_mm_json()
         else:
-            raise ValueError(f"Invalid output format: {self.output_format}")
+            raise ValueError(f'Invalid output format: {self.output_format}')
         return md_content
 
     def get_main_html(self):
--- tests/realcrawl/test_html_extract.py
+++ tests/realcrawl/test_html_extract.py
@@ -1,14 +1,18 @@
-from realcrawl.extract.html_extract import HtmlExtract
-import unittest
 import os
+import unittest
+
+from realcrawl.extract.html_extract import HtmlExtract
+
+
 class TestHtmlExtract(unittest.TestCase):
     def setUp(self):
         self.base_path = os.path.dirname(os.path.abspath(__file__))
 
     def test_html_extract(self):
-        html_extract = HtmlExtract(os.path.join(self.base_path, "assets/1.html"))
+        html_extract = HtmlExtract(os.path.join(self.base_path, 'assets/1.html'))
         html_content = html_extract.get_html_content()
         assert len(html_content) > 0
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     unittest.main()
